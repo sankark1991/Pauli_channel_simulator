1b0a6f43ef2488e3eb3ec355d5854d602c1aa5db
Krishanu Sankar <sankark1991@gmail.com>
2022-03-06 18:18:18 -0800

    implemented generation of parity check circuit

--- surface_code/BaseParityCheck.py
+++ surface_code/BaseParityCheck.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 class BaseParityCheck:
     """Rotated surface code with distance 5."""
@@ -8,9 +8,75 @@ class BaseParityCheck:
         Then for each edge (v1, v2), stores the list of faults (l, P) which trigger that edge."""
         pass
 
-    def circuit(self):
-        """Encodes the parity check circuit, and also stores the list of error locations/types (l, P)."""
-        pass
+    def get_circuit(self):
+        """Encodes the parity check circuit, and also stores the list of error locations/types (l, P).
+        The CNOTs can be scheduled so that:
+            - no syndrome qubit is ever idle
+            - data qubits in the bulk are never idle
+            - data qubits along the bottom and left edge are idle in round 1
+            - data qubits along the top and right edge are idle in round 4
+            - data qubit in the bottom left corner is isle in rounds 1 and 2
+            - data qubit in the bottom right and top left corners are idle in rounds 1 and 4
+            - data qubit in the top right corner is idle in rounds 3 and 4"""
+        self.data = [(2 * i, 2 * j) for j in range(5) for i in range(5)]
+        self.X_syns = [np.array([1 + 4 * i - 2 * k, 1 + 4 * j + 2 * k])
+                       for k in range(2) for j in range(2) for i in range(3)]
+        self.Z_syns = [np.array([1 + 4 * j + 2 * k, -1 + 4 * i + 2 * k])
+                       for k in range(2) for j in range(2) for i in range(3)]
+        # The part of the parity check circuit where noise can occur
+        round_0 = {'RX': self.X_syns, 'RZ': self.Z_syns, 'IDLE_R': v in self.data}
+
+        round_1 = {
+            'CNOT': [(v, v + np.array([1, 1])) for v in self.X_syns if v[0] < 8] + [(v, v + np.array([1, 1])) for v in
+                                                                                    self.Z_syns if v[1] < 8],
+            'IDLE_CNOT': [np.array([0, 0]), np.array([8, 0]), np.array([0, 8])]}
+
+        round_2 = {
+            'CNOT': [(v, v + np.array([-1, 1])) for v in self.X_syns if v[0] > 0] + [(v, v + np.array([1, -1])) for v in
+                                                                                     self.Z_syns if v[1] > 0],
+            'IDLE_CNOT': [np.array([0, 0])]}
+
+        round_3 = {
+            'CNOT': [(v, v + np.array([1, -1])) for v in self.X_syns if v[0] < 8] + [(v, v + np.array([-1, 1])) for v in
+                                                                                     self.Z_syns if v[1] < 8],
+            'IDLE_CNOT': [np.array([8, 8])]}
+
+        round_4 = {
+            'CNOT': [(v, v + np.array([-1, -1])) for v in self.X_syns if v[0] > 0] + [(v, v + np.array([-1, -1])) for v in
+                                                                                     self.Z_syns if v[1] > 0],
+            'IDLE_CNOT': [np.array([8, 0]), np.array([0, 8]), np.array([8, 8])]}
+
+        round_5 = {'MRX': self.X_syns, 'MRZ': self.Z_syns, 'IDLE_MR': self.data}
+
+        # The part of the parity check circuit which is perfect
+        round_6 = {
+            'CNOT': [(v, v + np.array([1, 1])) for v in self.X_syns if v[0] < 8] + [(v, v + np.array([1, 1])) for v in
+                                                                                    self.Z_syns if v[1] < 8]}
+
+        round_7 = {
+            'CNOT': [(v, v + np.array([-1, 1])) for v in self.X_syns if v[0] > 0] + [(v, v + np.array([1, -1])) for v in
+                                                                                     self.Z_syns if v[1] > 0]}
+
+        round_8 = {
+            'CNOT': [(v, v + np.array([1, -1])) for v in self.X_syns if v[0] < 8] + [(v, v + np.array([-1, 1])) for v in
+                                                                                     self.Z_syns if v[1] < 8]}
+
+        round_9 = {
+            'CNOT': [(v, v + np.array([-1, -1])) for v in self.X_syns if v[0] > 0] + [(v, v + np.array([-1, -1])) for v
+                                                                                      in
+                                                                                      self.Z_syns if v[1] > 0]}
+
+        round_10 = {'RX': self.X_syns, 'RZ': self.Z_syns}
+
+        # Store the circuit and fault locations
+        self.circuit = [round_0, round_1, round_2, round_3, round_4, round_5, round_6, round_7, round_8, round_9, round_10]
+        self.fault_locations = []
+        for round_number in range(6):
+            round = self.circuit[round_number]
+            for fault_type in round.keys():
+                target_qubits_list = round[fault_type]
+                for qbs in target_qubits_list:
+                    self.fault_locations.append((round_number, fault_type, qbs))
 
     def syn_diff(self, l, P):
         """Given an error (l, P), simulates the parity-check circuit followed by a perfect parity-check circuit,
